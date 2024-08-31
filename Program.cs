f8a400eb623caa39c563e63235358a8bd439ea2c
alaxgabriel71 <alaxgabriel71@gmail.com>
2024-08-31 09:05:43 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("1234", "Lumia 710", "abcd", 16);
+Iphone iphone = new Iphone("5678", "X", "efgh", 64);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
